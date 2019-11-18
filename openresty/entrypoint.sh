1b5f3da8f7e8d5b0af35fb1dea61619f5bd0e8ac
cxq777 <cxq777@gmail.com>
2019-11-18 12:38:27 +0800

    openresty auto reload

--- openresty/entrypoint.sh
+++ openresty/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/openresty -g "daemon off;" &
+/usr/local/openresty/bin/openresty -g "daemon off;" &
 
 inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T %f %e' \
 /usr/local/openresty/nginx/conf/conf.d | while read event; do
