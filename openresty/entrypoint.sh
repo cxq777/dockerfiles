cb53ce3635fd55e48671c07bd1969076d62bd545
cxq777 <cxq777@gmail.com>
2019-11-19 12:58:54 +0800

    openresty

--- openresty/entrypoint.sh
+++ openresty/entrypoint.sh
@@ -3,7 +3,7 @@
 /usr/local/openresty/bin/openresty -g "daemon off;" &
 
 inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T %f %e' \
-/usr/local/openresty/nginx/conf/conf.d | while read event; do
+/usr/local/openresty/nginx/conf | while read event; do
     echo "$event"
     /usr/local/openresty/nginx/sbin/nginx -s reload
 done
